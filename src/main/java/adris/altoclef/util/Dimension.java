44e88fd9170a2db4902fa31d168f1151ebb01834
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2026-05-15 00:15:31 -0400

    Create Dimension.java

--- src/main/java/adris/altoclef/util/Dimension.java
+++ src/main/java/adris/altoclef/util/Dimension.java
@@ -1,7 +1,19 @@
 package adris.altoclef.util;
 
-public enum Dimension {
-    OVERWORLD,
-    NETHER,
-    END
+public class Dimension {
+    public static final String OVERWORLD = "minecraft:overworld";
+    public static final String NETHER = "minecraft:the_nether";
+    public static final String END = "minecraft:the_end";
+
+    public static boolean isOverworld(String dimension) {
+        return OVERWORLD.equals(dimension);
+    }
+
+    public static boolean isNether(String dimension) {
+        return NETHER.equals(dimension);
+    }
+
+    public static boolean isEnd(String dimension) {
+        return END.equals(dimension);
+    }
 }
