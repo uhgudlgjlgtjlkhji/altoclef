69c39baf043aaf35a075b75f31f46092fee0b1d6
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2026-05-14 23:25:40 -0400

    Create mixin with official names - AbstractFurnaceScreenHandlerAccessor

--- src/main/java/adris/altoclef/mixins/AbstractFurnaceScreenHandlerAccessor.java
+++ src/main/java/adris/altoclef/mixins/AbstractFurnaceScreenHandlerAccessor.java
@@ -1,12 +1,20 @@
 package adris.altoclef.mixins;
 
 import net.minecraft.screen.AbstractFurnaceScreenHandler;
-import net.minecraft.screen.PropertyDelegate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(AbstractFurnaceScreenHandler.class)
 public interface AbstractFurnaceScreenHandlerAccessor {
-    @Accessor("propertyDelegate")
-    PropertyDelegate getPropertyDelegate();
+    @Accessor("fuelTime")
+    void setFuelTime(int fuelTime);
+    
+    @Accessor("fuelTime")
+    int getFuelTime();
+    
+    @Accessor("cookedTime")
+    void setCookedTime(int cookedTime);
+    
+    @Accessor("cookedTime")
+    int getCookedTime();
 }
