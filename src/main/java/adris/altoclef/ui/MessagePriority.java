438a7fff213bbea30f012c1a6a2d43b56bba0d7c
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2026-05-15 00:16:56 -0400

    Create MessagePriority.java

--- src/main/java/adris/altoclef/ui/MessagePriority.java
+++ src/main/java/adris/altoclef/ui/MessagePriority.java
@@ -1,18 +1,8 @@
 package adris.altoclef.ui;
 
-public enum MessagePriority {
-    ASAP(3),
-    TIMELY(2),
-    OPTIONAL(1),
-    UNAUTHORIZED(0);
-
-    private final int _importance;
-
-    MessagePriority(int importance) {
-        _importance = importance;
-    }
-
-    public int getImportance() {
-        return _importance;
-    }
+public class MessagePriority {
+    public static final int LOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int HIGH = 3;
+    public static final int CRITICAL = 4;
 }
