2a80cd1088b42c3efd6314c3746fd1dbc7e5178a
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2026-05-15 00:16:20 -0400

    Create WoodType.java

--- src/main/java/adris/altoclef/util/WoodType.java
+++ src/main/java/adris/altoclef/util/WoodType.java
@@ -1,12 +1,12 @@
 package adris.altoclef.util;
 
-public enum WoodType {
-    ACACIA,
-    BIRCH,
-    CRIMSON,
-    DARK_OAK,
-    OAK,
-    JUNGLE,
-    SPRUCE,
-    WARPED
+public class WoodType {
+    public static final String OAK = "oak";
+    public static final String SPRUCE = "spruce";
+    public static final String BIRCH = "birch";
+    public static final String JUNGLE = "jungle";
+    public static final String ACACIA = "acacia";
+    public static final String DARK_OAK = "dark_oak";
+    public static final String CRIMSON = "crimson";
+    public static final String WARPED = "warped";
 }
