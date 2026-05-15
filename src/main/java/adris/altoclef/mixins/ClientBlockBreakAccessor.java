6e0d56dc71ccbf39ab3a43ab2e5be3c233948b02
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2026-05-14 23:26:15 -0400

    Create mixin - ClientBlockBreakAccessor

--- src/main/java/adris/altoclef/mixins/ClientBlockBreakAccessor.java
+++ src/main/java/adris/altoclef/mixins/ClientBlockBreakAccessor.java
@@ -1,11 +1,11 @@
 package adris.altoclef.mixins;
 
-import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(ClientPlayerInteractionManager.class)
+@Mixin(BlockEntity.class)
 public interface ClientBlockBreakAccessor {
-    @Accessor("currentBreakingProgress")
-    float getCurrentBreakingProgress();
+    @Accessor("worldPos")
+    org.joml.Vector3i getWorldPos();
 }
