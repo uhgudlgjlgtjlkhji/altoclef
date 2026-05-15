d57d0191bdbff7247c74c6b5fc52bdc9ff37fe56
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2026-05-14 23:26:48 -0400

    Create mixin - ClientConnectionAccessor

--- src/main/java/adris/altoclef/mixins/ClientConnectionAccessor.java
+++ src/main/java/adris/altoclef/mixins/ClientConnectionAccessor.java
@@ -1,11 +1,12 @@
 package adris.altoclef.mixins;
 
-import net.minecraft.network.ClientConnection;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(ClientConnection.class)
+@Mixin(ChannelHandlerContext.class)
 public interface ClientConnectionAccessor {
-    @Accessor("ticks")
-    int getTicks();
+    @Accessor("channel")
+    Channel getChannel();
 }
