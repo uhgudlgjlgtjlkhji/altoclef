d195e2715c50ed067819cb2873f40d317474fa50
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2026-05-15 00:08:03 -0400

    Create ArgBase.java

--- src/main/java/adris/altoclef/commandsystem/ArgBase.java
+++ src/main/java/adris/altoclef/commandsystem/ArgBase.java
@@ -1,50 +1,25 @@
 package adris.altoclef.commandsystem;
 
-import java.lang.reflect.ParameterizedType;
+public class ArgBase {
+    protected final String name;
+    protected final boolean required;
+    protected final String description;
 
-public abstract class ArgBase {
-    protected int _minArgCountToUseDefault;
-    protected boolean _hasDefault;
-
-    protected <V> V getConverted(Class<V> vType, Object ob) {
-        try {
-            //noinspection unchecked
-            return (V) ob;
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Tried to convert the following object to type {typeof(V)} and failed: {ob}. This is probably an internal problem, contact the dev!");
-            //return default(T);
-        }
+    public ArgBase(String name, boolean required, String description) {
+        this.name = name;
+        this.required = required;
+        this.description = description;
     }
 
-    //public abstract Object ParseUnit ( String unit, String[] unitPlusRemainder );
-
-    @SuppressWarnings("unchecked")
-    public <V> V parseUnit(String unit, String[] unitPlusRemainder) throws CommandException {
-        // Fuck java
-        Class<V> vType = (Class<V>)
-                ((ParameterizedType) getClass()
-                        .getGenericSuperclass())
-                        .getActualTypeArguments()[0];
-
-        return getConverted(vType, parseUnit(unit, unitPlusRemainder));
+    public String getName() {
+        return name;
     }
 
-    public abstract <V> V getDefault(Class<V> vType);
-
-    public abstract String getHelpRepresentation();
-
-    public int getMinArgCountToUseDefault() {
-        return _minArgCountToUseDefault;
+    public boolean isRequired() {
+        return required;
     }
 
-    public boolean hasDefault() {
-        return _hasDefault;
+    public String getDescription() {
+        return description;
     }
-
-    public boolean isArray() {
-        return false;
-    }
-
-    public boolean isArbitrarilyLong() {return false;}
-
 }
