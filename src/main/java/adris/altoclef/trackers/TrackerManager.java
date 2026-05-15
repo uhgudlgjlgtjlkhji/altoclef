1284b02220084588adea5bd50d8aeaa68741cfdd
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2026-05-15 00:40:00 -0400

    Create TrackerManager.java

--- src/main/java/adris/altoclef/trackers/TrackerManager.java
+++ src/main/java/adris/altoclef/trackers/TrackerManager.java
@@ -1,41 +1,26 @@
 package adris.altoclef.trackers;
 
-import adris.altoclef.AltoClef;
-
+import adris.altoclef.tracker.Tracker;
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TrackerManager {
+    private final List<Tracker<?>> trackers = new ArrayList<>();
 
-    private final ArrayList<Tracker> _trackers = new ArrayList<>();
-
-    private final AltoClef _mod;
-
-    private boolean _wasInGame = false;
-
-    public TrackerManager(AltoClef mod) {
-        _mod = mod;
+    public <T> void register(Tracker<T> tracker) {
+        trackers.add(tracker);
     }
 
-    public void tick() {
-        boolean inGame = AltoClef.inGame();
-        if (!inGame && _wasInGame) {
-            // Reset when we leave our world
-            for (Tracker tracker : _trackers) {
-                tracker.reset();
-            }
-            // This is a a spaghetti. Fix at some point.
-            _mod.getChunkTracker().reset(_mod);
-            _mod.getMiscBlockTracker().reset();
-        }
-        _wasInGame = inGame;
-
-        for (Tracker tracker : _trackers) {
-            tracker.setDirty();
-        }
+    public void tickAll() {
+        trackers.forEach(Tracker::tick);
     }
 
-    public void addTracker(Tracker tracker) {
-        tracker._mod = _mod;
-        _trackers.add(tracker);
+    public void resetAll() {
+        trackers.forEach(Tracker::reset);
+    }
+
+    public List<Tracker<?>> getTrackers() {
+        return Collections.unmodifiableList(trackers);
     }
 }
