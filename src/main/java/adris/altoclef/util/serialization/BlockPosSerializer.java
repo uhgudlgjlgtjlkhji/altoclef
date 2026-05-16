b883cd566c8a0eab6d57bc6561cd44b48bc5f84a
uhgudlgjlgtjlkhji <swayam.happy1@gmail.com>
2026-05-16 20:48:21 +1000

    Update BlockPosSerializer.java

--- src/main/java/adris/altoclef/util/serialization/BlockPosSerializer.java
+++ src/main/java/adris/altoclef/util/serialization/BlockPosSerializer.java
@@ -1,13 +1,16 @@
 package adris.altoclef.util.serialization;
 
-import net.minecraft.util.math.BlockPos;
-
+import net.minecraft.core.BlockPos;
 import java.util.Arrays;
 import java.util.Collection;
 
 public class BlockPosSerializer extends AbstractVectorSerializer<BlockPos> {
+    public BlockPosSerializer() {
+        super(BlockPos.class);
+    }
+
     @Override
     protected Collection<String> getParts(BlockPos value) {
-        return Arrays.asList("" + value.getX(), "" + value.getY(), "" + value.getZ());
+        return Arrays.asList(String.valueOf(value.getX()), String.valueOf(value.getY()), String.valueOf(value.getZ()));
     }
 }
