a920de6d776422808042c0b9281aa854796ca664
uhgudlgjlgtjlkhji <swayam.happy1@gmail.com>
2026-05-16 20:48:53 +1000

    Refactor ChunkPosSerializer to use new ChunkPos import

--- src/main/java/adris/altoclef/util/serialization/ChunkPosSerializer.java
+++ src/main/java/adris/altoclef/util/serialization/ChunkPosSerializer.java
@@ -1,13 +1,16 @@
 package adris.altoclef.util.serialization;
 
-import net.minecraft.util.math.ChunkPos;
-
+import net.minecraft.world.level.ChunkPos;
 import java.util.Arrays;
 import java.util.Collection;
 
 public class ChunkPosSerializer extends AbstractVectorSerializer<ChunkPos> {
+    public ChunkPosSerializer() {
+        super(ChunkPos.class);
+    }
+
     @Override
     protected Collection<String> getParts(ChunkPos value) {
-        return Arrays.asList("" + value.x, "" + value.z);
+        return Arrays.asList(String.valueOf(value.x), String.valueOf(value.z));
     }
 }
