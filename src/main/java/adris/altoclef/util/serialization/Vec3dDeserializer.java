76781d7780dd6c9e88c3a23fdf1ab36601bc18a6
uhgudlgjlgtjlkhji <swayam.happy1@gmail.com>
2026-05-16 20:49:56 +1000

    Update Vec3dDeserializer.java

--- src/main/java/adris/altoclef/util/serialization/Vec3dDeserializer.java
+++ src/main/java/adris/altoclef/util/serialization/Vec3dDeserializer.java
@@ -1,34 +1,27 @@
 package adris.altoclef.util.serialization;
 
 import com.fasterxml.jackson.core.JsonToken;
-import net.minecraft.util.math.Vec3d;
-
+import net.minecraft.world.phys.Vec3;
 import java.util.List;
 
-public class Vec3dDeserializer extends AbstractVectorDeserializer<Vec3d, Double> {
-    @Override
-    protected String getTypeName() {
-        return "Vec3d";
+public class Vec3dDeserializer extends AbstractVectorDeserializer<Vec3, Double> {
+    public Vec3dDeserializer() {
+        super(Vec3.class);
     }
 
     @Override
-    protected String[] getComponents() {
-        return new String[]{"x", "y"};
-    }
-
-    @Override
-    protected Double parseUnit(String unit) throws Exception {
-        return Double.parseDouble(unit);
-    }
-
-    @Override
-    protected Vec3d deserializeFromUnits(List<Double> units) {
-        return new Vec3d(units.get(0), units.get(1), units.get(2));
+    protected Vec3 deserializeFromUnits(List<Double> units) {
+        if (units.size() < 3) return Vec3.ZERO;
+        return new Vec3(units.get(0), units.get(1), units.get(2));
     }
 
     @Override
     protected boolean isUnitTokenValid(JsonToken token) {
-        return token == JsonToken.VALUE_NUMBER_INT || token == JsonToken.VALUE_NUMBER_FLOAT;
+        return token == JsonToken.VALUE_NUMBER_FLOAT || token == JsonToken.VALUE_NUMBER_INT;
     }
 
+    @Override
+    protected Double parseUnit(String s) {
+        return Double.parseDouble(s.trim());
+    }
 }
