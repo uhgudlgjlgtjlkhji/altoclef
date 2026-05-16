1053670a9c273d16f76a480fe66ad6a72df792ff
uhgudlgjlgtjlkhji <swayam.happy1@gmail.com>
2026-05-16 20:50:13 +1000

    Refactor ItemSerializer to handle single Item serialization

--- src/main/java/adris/altoclef/util/serialization/ItemSerializer.java
+++ src/main/java/adris/altoclef/util/serialization/ItemSerializer.java
@@ -1,31 +1,24 @@
 package adris.altoclef.util.serialization;
 
-import adris.altoclef.util.helpers.ItemHelper;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import net.minecraft.item.Item;
-
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Item;
 import java.io.IOException;
-import java.util.List;
 
 public class ItemSerializer extends StdSerializer<Object> {
     public ItemSerializer() {
-        this(null);
-    }
-
-    public ItemSerializer(Class<Object> vc) {
-        super(vc);
+        super(Object.class);
     }
 
     @Override
     public void serialize(Object value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-        List<Item> items = (List<Item>) value;
-        gen.writeStartArray();
-        for (Item item : items) {
-            String key = ItemHelper.trimItemName(item.getTranslationKey());
-            gen.writeString(key);
+        if (value instanceof Item item) {
+            var key = BuiltInRegistries.ITEM.getKey(item);
+            gen.writeString(key != null ? key.toString() : "minecraft:air");
+        } else {
+            gen.writeNull();
         }
-        gen.writeEndArray();
     }
 }
