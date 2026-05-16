ca3936fa82d15d8f493dc57bbfb687a3224eb1ce
uhgudlgjlgtjlkhji <swayam.happy1@gmail.com>
2026-05-16 20:48:32 +1000

    Update BlockPosDeserializer.java

--- src/main/java/adris/altoclef/util/serialization/BlockPosDeserializer.java
+++ src/main/java/adris/altoclef/util/serialization/BlockPosDeserializer.java
@@ -1,33 +1,27 @@
 package adris.altoclef.util.serialization;
 
 import com.fasterxml.jackson.core.JsonToken;
-import net.minecraft.util.math.BlockPos;
-
+import net.minecraft.core.BlockPos;
 import java.util.List;
 
 public class BlockPosDeserializer extends AbstractVectorDeserializer<BlockPos, Integer> {
-    @Override
-    protected String getTypeName() {
-        return "BlockPos";
-    }
-
-    @Override
-    protected String[] getComponents() {
-        return new String[]{"x", "y", "z"};
-    }
-
-    @Override
-    protected Integer parseUnit(String unit) throws Exception {
-        return Integer.parseInt(unit);
+    public BlockPosDeserializer() {
+        super(BlockPos.class);
     }
 
     @Override
     protected BlockPos deserializeFromUnits(List<Integer> units) {
+        if (units.size() < 3) return BlockPos.ZERO;
         return new BlockPos(units.get(0), units.get(1), units.get(2));
     }
 
     @Override
     protected boolean isUnitTokenValid(JsonToken token) {
         return token == JsonToken.VALUE_NUMBER_INT;
+    }
+
+    @Override
+    protected Integer parseUnit(String s) {
+        return Integer.parseInt(s.trim());
     }
 }
