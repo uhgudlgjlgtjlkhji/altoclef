c98cc1a0da6d26460c9e79555fae64985d9b12f4
uhgudlgjlgtjlkhji <swayam.happy1@gmail.com>
2026-05-16 20:47:43 +1000

    Update AbstractVectorSerializer.java

--- src/main/java/adris/altoclef/util/serialization/AbstractVectorSerializer.java
+++ src/main/java/adris/altoclef/util/serialization/AbstractVectorSerializer.java
@@ -3,24 +3,22 @@ package adris.altoclef.util.serialization;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-
 import java.io.IOException;
 import java.util.Collection;
 
 public abstract class AbstractVectorSerializer<T> extends StdSerializer<T> {
-    public AbstractVectorSerializer() {
-        this(null);
-    }
-
-    public AbstractVectorSerializer(Class<T> vc) {
-        super(vc);
+    protected AbstractVectorSerializer(Class<T> t) {
+        super(t);
     }
 
     protected abstract Collection<String> getParts(T value);
 
     @Override
     public void serialize(T value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-        Collection<String> parts = getParts(value);
-        gen.writeString(String.join(",", parts));
+        gen.writeStartArray();
+        for (String part : getParts(value)) {
+            gen.writeString(part);
+        }
+        gen.writeEndArray();
     }
 }
