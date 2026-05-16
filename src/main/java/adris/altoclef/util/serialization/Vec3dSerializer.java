f60a565b7f2fc1a34745d4add513a7350d271810
uhgudlgjlgtjlkhji <swayam.happy1@gmail.com>
2026-05-16 20:49:43 +1000

    Update Vec3dSerializer.java

--- src/main/java/adris/altoclef/util/serialization/Vec3dSerializer.java
+++ src/main/java/adris/altoclef/util/serialization/Vec3dSerializer.java
@@ -1,13 +1,16 @@
 package adris.altoclef.util.serialization;
 
-import net.minecraft.util.math.Vec3d;
-
+import net.minecraft.world.phys.Vec3;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class Vec3dSerializer extends AbstractVectorSerializer<Vec3d> {
+public class Vec3dSerializer extends AbstractVectorSerializer<Vec3> {
+    public Vec3dSerializer() {
+        super(Vec3.class);
+    }
+
     @Override
-    protected Collection<String> getParts(Vec3d value) {
-        return Arrays.asList("" + value.getX(), "" + value.getY(), "" + value.getZ());
+    protected Collection<String> getParts(Vec3 value) {
+        return Arrays.asList(String.valueOf(value.x), String.valueOf(value.y), String.valueOf(value.z));
     }
 }
